b1c4476a4eebea9e11c0868bdadd987539876add
yujicosao <yujikosao@me.com>
2019-11-12 09:26:38 +0900

    first commit

---
+++ pages/about.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h1>About DadJokes</h1>
+        <p>This is an app that display dad joeks</p>
+    </div>
+</template>
+
+<script>
+export default {
+  head(){
+      return {
+          title: 'About The App',
+          meta:[
+              {
+                  hid: 'description',
+                  name: 'description',
+                  content: 'Best place for corny dad jokes'
+              }
+          ]
+      }
+  }  
+};
+</script>
+<style lang="stylus">
+
+</style>
